bf3eb3b342bad4b22aa42a226f9f44efcb1659f0
s241977 <bluepaddlepop@gmail.com>
2026-02-13 10:11:46 +1100

    Configured raylib

--- RaylibC-BrickBreaker/main.cpp
+++ RaylibC-BrickBreaker/main.cpp
@@ -1,9 +1,24 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <raylib.h>
+
 int main(int argC, char* argV[])
 {
-	std::cout << "Hello, World!\n";
+	InitWindow(1080, 720, "BrickBreaker");
 
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground((RAYWHITE));
+
+		DrawCircle(540, 200, 93.2f, GREEN);
+		DrawRectangle(110, 35, 12, 41, ORANGE);
+		
+		EndDrawing();
+	}
+	
+	CloseWindow();
+	
 	return EXIT_SUCCESS;
 }
